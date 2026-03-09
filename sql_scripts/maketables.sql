c0a7fc434f710f94d368bc1ea971dc38b322fb45
algra2001 <dandush1001@gmail.com>
2026-03-09 22:19:46 +0000

    Updated database tables

--- sql_scripts/maketables.sql
+++ sql_scripts/maketables.sql
@@ -1,10 +1,10 @@
 # Adapted from class code
 CREATE DATABASE IF NOT EXISTS s2883992_web_project;
 USE s2883992_web_project;
-DROP TABLE seq_group;
-DROP TABLE sequences;
-DROP TABLE jobs;
-DROP TABLE queries;
+#DROP TABLE seq_group;
+#DROP TABLE sequences;
+#DROP TABLE jobs;
+#DROP TABLE queries;
 
 # queries table for unique query combinations (protein and taxon)
 CREATE TABLE `queries` (
@@ -27,19 +27,64 @@ PRIMARY KEY(`accession`)
 # mapping table for queries and sequences
 CREATE TABLE `seq_group` (
 `query_id` INT UNSIGNED NOT NULL,
-`seq_id` VARCHAR(255) NOT NULL,
-PRIMARY KEY (`query_id`, `seq_id`), # unique combinations of query and sequence, for sequences in multiple queries
+`accession` VARCHAR(255) NOT NULL,
+#`clustalo_seq` LONGTEXT NOT NULL, 
+PRIMARY KEY (`query_id`, `accession`), # unique combinations of query and sequence, for sequences in multiple queries
 FOREIGN KEY (`query_id`) REFERENCES `queries`(`query_id`) ON DELETE CASCADE, # link to queries table
-FOREIGN KEY (`seq_id`) REFERENCES `sequences`(`accession`) ON DELETE CASCADE # link to sequences table
+FOREIGN KEY (`accession`) REFERENCES `sequences`(`accession`) ON DELETE CASCADE # link to sequences table
 );
 
 # table for connecting users and queries
 CREATE TABLE `jobs` (
-`job_id` INT UNSIGNED NOT NULL AUTO_INCREMENT KEY,
+`job_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
 `user_id` VARCHAR(255) NOT NULL,
 `query_id` INT UNSIGNED NOT NULL,
 `job_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 `status` ENUM('pending', 'complete', 'error') DEFAULT 'pending',
+PRIMARY KEY (`job_id`),
 FOREIGN KEY (`query_id`) REFERENCES `queries`(`query_id`) ON DELETE CASCADE
 );
+
+# table for analysis output
+CREATE TABLE `analysis_outputs` (
+`output_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+`job_id` INT UNSIGNED NOT NULL,
+`analysis_type` ENUM('msa', 'plotcon', 'motifs') NOT NULL,
+`file_path` VARCHAR(255) NOT NULL,
+`parameters` TEXT NULL,
+`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (`output_id`),
+FOREIGN KEY (`job_id`) REFERENCES `jobs`(`job_id`) ON DELETE CASCADE
+);
+
+# table for alignment results
+CREATE TABLE `aligned_sequences` (
+`align_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+`job_id` INT UNSIGNED NOT NULL,
+`accession` VARCHAR(255) NOT NULL,
+`aligned_sequence` LONGTEXT NOT NULL,
+PRIMARY KEY (`align_id`),
+FOREIGN KEY (`job_id`) REFERENCES jobs(`job_id`) ON DELETE CASCADE,
+FOREIGN KEY (`accession`) REFERENCES sequences(`accession`) ON DELETE CASCADE
+);
+
+# table for motifs
+CREATE TABLE `motif_hits` (
+`hit_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+`job_id` INT UNSIGNED NOT NULL,
+`accession` VARCHAR(255) NOT NULL,
+`motif_name` VARCHAR(255) NOT NULL,
+`start_pos` INT UNSIGNED NOT NULL,
+`end_pos` INT UNSIGNED NOT NULL,
+`score` INT NULL,
+`strand` ENUM('+', '-') NOT NULL, # Maybe ignore this because we have protein seqs
+`matched_sequence` VARCHAR(255) NULL,
+`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (`hit_id`),
+    FOREIGN KEY (`job_id`) REFERENCES `jobs`(`job_id`) ON DELETE CASCADE,
+    FOREIGN KEY (`accession`) REFERENCES `sequences`(`accession`) ON DELETE CASCADE,
+    INDEX (`job_id`),
+    INDEX (`accession`)
+);
+
 
