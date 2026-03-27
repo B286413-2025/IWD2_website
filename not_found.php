5a8ef07c7fedf486da36143a4b5df991688a18fe
algra2001 <dandush1001@gmail.com>
2026-03-27 13:39:50 +0000

    Minimal 404 page

---
+++ not_found.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+require_once 'set_cookies.php';
+
+echo <<<_HTML
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<title>Page Not Found</title>
+</head>
+<body>
+_HTML;
+
+include 'menuf.php';
+
+echo <<<_BODY
+<h1>404 - Page Not Found</h1>
+<p>Sorry, the page you requested could not be found on this website.</p>
+<p>You can use the menu options or return to the front page.</p>
+<form action="front.php" method="get">
+  <button type="submit">Front Page</button>
+</form>
+</body>
+</html>
+_BODY;
+?>
+
